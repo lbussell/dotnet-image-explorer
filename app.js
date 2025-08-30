06e0f79a509920d87ac3b587a7280a2ce21ba21a
Logan Bussell <loganbussell@microsoft.com>
2025-08-29 21:45:10 -0700

    Do some cleanup

--- app.js
+++ app.js
@@ -4,41 +4,20 @@
 window.onload = main;
 
 async function main() {
-  // These elements are updated later below
-  const elements = {
-    ref: document.getElementById("ref"),
-    branch: document.getElementById("branch"),
-    repos: document.getElementById("repos"),
-  };
 
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const ref = urlSearchParams.get("ref") ?? "refs/heads/main";
-  const branch = urlSearchParams.get("branch") ?? "main";
-
-  if (elements.ref) {
-    elements.ref.textContent = "Ref: " + ref;
+  const reposElement = document.getElementById("repos");
+  if (!reposElement) {
+    return;
   }
 
-  if (elements.branch) {
-    elements.branch.textContent = "Branch: " + branch;
-  }
+  const imageInfo = await getImageInfo();
 
-  const imageInfo = await getImageInfoJson(ref, branch);
-  renderImageInfo(imageInfo);
-
-  function renderImageInfo(imageInfo) {
-    const reposElement = elements.repos;
-    if (!reposElement) {
-      return;
-    }
-
-    imageInfo.repos.forEach(repo => {
-      const repoElement = document.createElement("li");
-      const repoHtml = renderRepoHtml(repo);
-      repoElement.innerHTML = repoHtml;
-      reposElement.appendChild(repoElement);
-    });
-  }
+  imageInfo.repos.forEach(repo => {
+    const repoElement = document.createElement("li");
+    const repoHtml = renderRepoHtml(repo);
+    repoElement.innerHTML = repoHtml;
+    reposElement.appendChild(repoElement);
+  });
 }
 
 function renderRepoHtml(repo) {
@@ -46,17 +25,8 @@ function renderRepoHtml(repo) {
   return `<strong>${repo.repo}</strong> - ${numberOfImages} images`;
 }
 
-async function getImageInfoJson(ref, branch) {
-  // Example URLs:
-  // https://raw.githubusercontent.com/dotnet/versions/refs/heads/main/build-info/docker/image-info.dotnet-dotnet-docker-main.json
-  // https://raw.githubusercontent.com/dotnet/versions/273efa8f821c43a6f286dca1a99b26a6b5c7c5f4/build-info/docker/image-info.dotnet-dotnet-docker-main.json
-  const file = `build-info/docker/image-info.dotnet-dotnet-docker-${branch}.json`;
-  const imageInfoUrl = `https://raw.githubusercontent.com/dotnet/versions/${ref}/${file}`;
-  const imageInfo = await fetchJson(imageInfoUrl);
-  return imageInfo;
-}
-
-async function fetchJson(url) {
+async function getImageInfo() {
+  const url = "image-info.dotnet-dotnet-docker-main.json"
   const response = await fetch(url);
   return await response.json();
 }
